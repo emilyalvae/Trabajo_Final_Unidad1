6142030a21aeb7263ceaa93e5c0c37a4abd47274
brian24C <brianjosuecastro@hotmail.com>
2022-11-05 13:10:38 -0500

    mejorando menu de tarea 2

--- tarea2.py
+++ tarea2.py
@@ -144,30 +144,40 @@ def tipo():
     pass
 
 
+def menu():
+    print("----------------------------------------------------MENU---------------------------------------------------")
 
-print("----------------------------------------------------MENU---------------------------------------------------")
-
-print("""Opción 1: Listar pokemons por generación.
-Opción 2: Listar pokemons por forma. 
-Opción 3: Listar pokemons por habilidad. 
-Opción 4: Listar pokemons por habitat. .
-Opción 5: Listar pokemons por tipo. """)
+    print("""Opción 1: Listar pokemons por generación.
+    Opción 2: Listar pokemons por forma. 
+    Opción 3: Listar pokemons por habilidad. 
+    Opción 4: Listar pokemons por habitat. .
+    Opción 5: Listar pokemons por tipo. """)
 
 
-opcion=int(input("INGRESE UNA OPCION DEL 1 AL 5: "))
-opciones=[1,2,3,4,5]
+    opcion=input("INGRESE UNA OPCION DEL 1 AL 5: ")
+    opciones=["1","2","3","4","5"]
 
-while opcion not in opciones:
-    opcion=int(input("INGRESE UNA OPCION VALIDA DEL 1 AL 5: "))
+    while opcion not in opciones:
+        opcion=input("INGRESE UNA OPCION VALIDA DEL 1 AL 5: ")
 
-if opcion==1:
-    generacion()
-if opcion==2:
-    forma()
-if opcion==3:
-    habilidad()
-if opcion==4:
-    habitat()
-if opcion==5:
-    tipo()
 
+    if opcion=="1":
+        generacion()
+    if opcion=="2":
+        forma()
+    if opcion=="3":
+        habilidad()
+    if opcion=="4":
+        habitat()
+    if opcion=="5":
+        tipo()
+
+    
+menu()
+
+print("Presione 'y' para continuar y cualquier letra para salir del programa")
+continuar=input("Desea continuar con las opciones? (y/n): ")
+
+while continuar=="y":
+    menu()
+    continuar=input("Desea continuar con las opciones? (y/n): ")
